3ccf017ee235134f1916bed9b39e4f2385f03012
usama <usamafodkar@gmail.com>
2024-06-02 23:33:35 +0530

    While loop update

--- 13_While_loops.py
+++ 13_While_loops.py
@@ -12,4 +12,20 @@ else:
 
 # break: Breaks out of the current closest enclosing loop
 # continue: Goes to the top of the closest closest enclosing loop
-# pass: Does nothing at all
+# pass: Does nothing at all (like a place holder)
+
+x = 0               #initiate a variable
+while x < 5:
+    x += 1          #increment
+    if x == 3:      #condition
+        x += 1      #increment and continue
+        continue
+    print(f'The current value of x is {x}')
+    
+
+x = 0               #initiate a variable
+while x < 5:
+    x += 1          #increment
+    if x == 4:      #condition
+        break       #Break
+    print(f'The current value of x is {x}')
